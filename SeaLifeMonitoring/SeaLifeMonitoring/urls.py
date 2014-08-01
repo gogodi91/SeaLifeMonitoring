a9c029c961aae38c702af132c62bba90b53558b2
Georgi Dimitrov <gogodi91@gmail.com>
2014-08-01 16:20:18 +0300

    final commit of the day (media server created)

--- SeaLifeMonitoring/SeaLifeMonitoring/urls.py
+++ SeaLifeMonitoring/SeaLifeMonitoring/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,10 @@ urlpatterns = patterns('',
     url(r'^Monitoring/', include('Monitoring.urls')), #new tuple
     url(r'', include('Monitoring.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
